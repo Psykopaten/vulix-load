05a2c1fd5f8f6b291cd6ef1c8d7d7844444d8ced
Psykopaten <104300989+Psykopaten@users.noreply.github.com>
2022-07-25 13:05:24 +0200

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,17 @@
+fx_version 'cerulean'
+games { 'gta5' }
+
+version '1.0.5'
+author 'Psykopaten'
+description 'LoadScreen P xd'
+
+loadscreen 'index.html'
+
+
+files {
+    'index.html',
+    'main.css',
+    "xd.mp3",
+    "img/discord.png",
+    "img/logo.png"
+}
